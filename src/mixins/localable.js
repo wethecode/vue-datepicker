2aee4e117599f426d3db9be76817ab7f2124b3e3
wethecode <null@wethecode.com>
2021-12-03 22:36:34 +0100

    (temp) fix: locale infinite loop / maximum call stack size exceeded bug
    build
    
    comment added + cleanup

--- src/mixins/localable.js
+++ src/mixins/localable.js
@@ -31,7 +31,28 @@ export default Vue.extend({
         properties.every(property => DEFAULT_LOCALE_PROPERTIES.includes(property));
     },
     getLocale (lang) {
-      return this.isValidLocale(lang) ? lang : locales[lang] || this.getLocale(this.getDefaultLang());
+      /**
+       * At some point `lang` parameter becomes undefined (with `locale` prop's default value -- this should be fixed)
+       * so it tries to `getDefaultLang`. If navigator's locale is not in `locales` the infinite recursive loop bug happens
+       * which causes the 'Maximum call stack size exceeded' error.
+       * Also `lang` param can be an object or a string so `isValidLocale` should not be called every time.
+       **/
+      // return this.isValidLocale(lang) ? lang : locales[lang] || this.getLocale(this.getDefaultLang());
+
+      if (typeof lang === 'object' && this.isValidLocale(lang)) {
+        return lang;
+      } else if (typeof lang === 'string') {
+        if (locales[lang]) {
+          return locales[lang];
+        } else {
+          let defaultLang = this.getDefaultLang();
+          if (locales[defaultLang]) {
+            return locales[defaultLang];
+          }
+        }
+      }
+      // Fallback to 'en' locale
+      return locales['en'];
     },
   },
 });
